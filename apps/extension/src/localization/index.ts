999545ec9618f08127a0e8245effb4f25b27fe6c
Agustin Barrientos <39675693+agustinbarrientos@users.noreply.github.com>
2026-09-04 10:21:20 -0500

    Export extension localization utilities

---
+++ apps/extension/src/localization/index.ts
@@ -0,0 +1,7 @@
+export { SupportedLanguages } from './catalogs';
+export {
+	createLocalizationBundle,
+	loadLocalizationBundle,
+	type DocumentCopy,
+	type LocalizationBundle,
+} from './utils/create-localization-bundle';
